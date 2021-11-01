11aa0b4e089f169dcdbf2cb8eecc61c3273c0b66
Matheus Junior Menezes da Paixão <53098231+MatheusJMP@users.noreply.github.com>
2021-11-01 15:48:52 -0300

    Criação da tela principal, e inicio da implementação da bottom navigation

--- app/src/main/java/com/example/study_evo/MainActivity.kt
+++ app/src/main/java/com/example/study_evo/MainActivity.kt
@@ -2,10 +2,25 @@ package com.example.study_evo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var recyclerAdapter : RecyclerViewAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        recyclerAdapter = RecyclerViewAdapter(fakeCards())
+        recycler_view_main.adapter = recyclerAdapter
+        recycler_view_main.layoutManager = LinearLayoutManager(this)
+
+        val images = listOf(
+            R.drawable.ic_creditcard,
+            R.drawable.ic_creditcard2
+        )
+        val adapter = ViewPagerAdapter(images)
+        view_pager.adapter = adapter
+
     }
 }
